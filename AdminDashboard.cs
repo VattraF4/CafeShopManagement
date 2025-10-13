88bd689876262769ce95b08c3c8ed7504d02cc6e
Yan Pich <yanpich154@gmail.com>
2025-10-13 15:36:14 +0700

    Revert "update name form"
    This reverts commit e2f06e337086f1897a2ab8ccc47d0b2721c23cae.

--- AdminDashboard.cs
+++ AdminDashboard.cs
@@ -10,56 +10,11 @@ using System.Windows.Forms;
 
 namespace OOADCafeShopManagement
 {
-    public partial class frmDashboard : Form
+    public partial class AdminDashboard : Form
     {
-        //Config for moving form
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern bool ReleaseCapture();
-
-        public frmDashboard()
+        public AdminDashboard()
         {
             InitializeComponent();
-            MakeDraggable(pnlTitleBar);
-            MakeDraggable(lbTitle);
-            // Attach events to lbX label
-            lbX.MouseEnter += lbX_MouseEnter;
-            lbX.MouseLeave += lbX_MouseLeave;
-            lbX.Click += lbX_Click;
-        }
-
-        //use to move window form by properties tools or control box
-        private void MakeDraggable(Control control)
-        {
-            control.MouseDown += (s, e) =>
-            {
-                if (e.Button == MouseButtons.Left)
-                {
-                    ReleaseCapture();
-                    SendMessage(Handle, 0xA1, 0x2, 0);
-                }
-            };
-        }
-        private void lbX_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void lbX_MouseEnter(object sender, EventArgs e)
-        {
-            lbX.ForeColor = Color.Red;
-        }
-        private void lbX_MouseLeave(object sender, EventArgs e)
-        {
-            lbX.ForeColor = Color.White; // Change to your original color
-        }
-
-        private void btnDashboard_Click(object sender, EventArgs e)
-        {
-            Dashboard adminDashboard = new Dashboard();
-            adminDashboard.Show();
-            this.Hide();
         }
     }
 }
--- Dashboard.cs
+++ Dashboard.cs
@@ -10,11 +10,51 @@ using System.Windows.Forms;
 
 namespace OOADCafeShopManagement
 {
-    public partial class Dashboard : Form
+    public partial class frmDashboard : Form
     {
-        public Dashboard()
+        //Config for moving form
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public frmDashboard()
         {
             InitializeComponent();
+            MakeDraggable(pnlTitleBar);
+            MakeDraggable(lbTitle);
+            // Attach events to lbX label
+            lbX.MouseEnter += lbX_MouseEnter;
+            lbX.MouseLeave += lbX_MouseLeave;
+            lbX.Click += lbX_Click;
         }
+
+        //use to move window form by properties tools or control box
+        private void MakeDraggable(Control control)
+        {
+            control.MouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    ReleaseCapture();
+                    SendMessage(Handle, 0xA1, 0x2, 0);
+                }
+            };
+        }
+        private void lbX_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lbX_MouseEnter(object sender, EventArgs e)
+        {
+            lbX.ForeColor = Color.Red;
+        }
+        private void lbX_MouseLeave(object sender, EventArgs e)
+        {
+            lbX.ForeColor = Color.White; // Change to your original color
+        }
+
+
     }
 }
