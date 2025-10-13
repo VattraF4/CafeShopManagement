e2f06e337086f1897a2ab8ccc47d0b2721c23cae
vattra.f4 <vattraramo13@gmail.com>
2025-10-13 12:44:24 +0700

    update name form

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OOADCafeShopManagement
 {
-    partial class frmDashboard
+    partial class Dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+       
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -31,31 +32,21 @@
             this.pnlTitleBar = new System.Windows.Forms.Panel();
             this.lbX = new System.Windows.Forms.Label();
             this.lbTitle = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnAddCashier = new System.Windows.Forms.Button();
-            this.btnAddProducts = new System.Windows.Forms.Button();
-            this.btnAddCustomer = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pnlTitleBar.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlTitleBar
             // 
             this.pnlTitleBar.AutoScroll = true;
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
             this.pnlTitleBar.Controls.Add(this.lbX);
             this.pnlTitleBar.Controls.Add(this.lbTitle);
             this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
             this.pnlTitleBar.Name = "pnlTitleBar";
-            this.pnlTitleBar.Size = new System.Drawing.Size(745, 32);
-            this.pnlTitleBar.TabIndex = 1;
+            this.pnlTitleBar.Size = new System.Drawing.Size(756, 32);
+            this.pnlTitleBar.TabIndex = 3;
             // 
             // lbX
             // 
@@ -67,7 +58,6 @@
             this.lbX.Size = new System.Drawing.Size(16, 15);
             this.lbX.TabIndex = 1;
             this.lbX.Text = "X";
-            this.lbX.Click += new System.EventHandler(this.lbX_Click);
             // 
             // lbTitle
             // 
@@ -80,177 +70,40 @@
             this.lbTitle.TabIndex = 0;
             this.lbTitle.Text = "Cafe Shop Management System";
             // 
-            // panel2
+            // panel1
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(45)))));
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.btnAddCustomer);
-            this.panel2.Controls.Add(this.btnAddProducts);
-            this.panel2.Controls.Add(this.btnAddCashier);
-            this.panel2.Controls.Add(this.btnDashboard);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 32);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(256, 455);
-            this.panel2.TabIndex = 2;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(45)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(732, 137);
+            this.panel1.TabIndex = 4;
             // 
-            // btnDashboard
-            // 
-            this.btnDashboard.BackColor = System.Drawing.Color.Transparent;
-            this.btnDashboard.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDashboard.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnDashboard.Location = new System.Drawing.Point(39, 153);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(171, 37);
-            this.btnDashboard.TabIndex = 2;
-            this.btnDashboard.Text = "Dashboard";
-            this.btnDashboard.UseVisualStyleBackColor = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Transparent;
-            this.label5.Location = new System.Drawing.Point(127, 117);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 12);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Admin";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(37, 117);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 12);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Username : ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(74, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 17);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "User\'s Portal";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::OOADCafeShopManagement.Properties.Resources.cafe;
-            this.pictureBox1.Location = new System.Drawing.Point(100, 25);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // btnAddCashier
-            // 
-            this.btnAddCashier.BackColor = System.Drawing.Color.Transparent;
-            this.btnAddCashier.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnAddCashier.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btnAddCashier.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAddCashier.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddCashier.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnAddCashier.Location = new System.Drawing.Point(39, 196);
-            this.btnAddCashier.Name = "btnAddCashier";
-            this.btnAddCashier.Size = new System.Drawing.Size(171, 37);
-            this.btnAddCashier.TabIndex = 2;
-            this.btnAddCashier.Text = "Add Cashiers";
-            this.btnAddCashier.UseVisualStyleBackColor = false;
-            // 
-            // btnAddProducts
-            // 
-            this.btnAddProducts.BackColor = System.Drawing.Color.Transparent;
-            this.btnAddProducts.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnAddProducts.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btnAddProducts.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAddProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddProducts.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnAddProducts.Location = new System.Drawing.Point(39, 239);
-            this.btnAddProducts.Name = "btnAddProducts";
-            this.btnAddProducts.Size = new System.Drawing.Size(171, 37);
-            this.btnAddProducts.TabIndex = 2;
-            this.btnAddProducts.Text = "Add Products";
-            this.btnAddProducts.UseVisualStyleBackColor = false;
-            // 
-            // btnAddCustomer
-            // 
-            this.btnAddCustomer.BackColor = System.Drawing.Color.Transparent;
-            this.btnAddCustomer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnAddCustomer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btnAddCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAddCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddCustomer.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnAddCustomer.Location = new System.Drawing.Point(39, 282);
-            this.btnAddCustomer.Name = "btnAddCustomer";
-            this.btnAddCustomer.Size = new System.Drawing.Size(171, 37);
-            this.btnAddCustomer.TabIndex = 2;
-            this.btnAddCustomer.Text = "Add Customer";
-            this.btnAddCustomer.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
-            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.button4.Location = new System.Drawing.Point(39, 406);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(171, 37);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "LOG OUT";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // frmDashboard
+            // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(745, 487);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(756, 445);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlTitleBar);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "frmDashboard";
+            this.Name = "AdminDashboard";
             this.Opacity = 0.95D;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "AdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AdminDashboard";
             this.pnlTitleBar.ResumeLayout(false);
             this.pnlTitleBar.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
         private System.Windows.Forms.Panel pnlTitleBar;
-        private System.Windows.Forms.Label lbTitle;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lbX;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnDashboard;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button btnAddCustomer;
-        private System.Windows.Forms.Button btnAddProducts;
-        private System.Windows.Forms.Button btnAddCashier;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Panel panel1;
     }
 }
